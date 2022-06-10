e934a62a6481d7c9fabce245f3c17832d5cbe090
cliffordc <cliffordhc@gmail.com>
2022-06-10 10:29:19 +0200

    Update gen.sh to use top level gradle

--- gen.sh
+++ gen.sh
@@ -3,8 +3,7 @@
 # example 
 # gen.sh https://play.dhis2.org/2.37.6
 
-cd generate
-./gradlew clean build
+/gradlew clean build
 cd -
 java -jar generate/build/libs/generate-0.0.1-SNAPSHOT.jar "$@"
 ./_genonce.sh
