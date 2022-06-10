dd70e60fa534eddaef501ef7a67261ec0140e711
cliffordc <cliffordhc@gmail.com>
2022-06-10 10:34:01 +0200

    Fix wrong gradlew path

--- gen.sh
+++ gen.sh
@@ -3,7 +3,6 @@
 # example 
 # gen.sh https://play.dhis2.org/2.37.6
 
-/gradlew clean build
-cd -
+./gradlew clean build
 java -jar generate/build/libs/generate-0.0.1-SNAPSHOT.jar "$@"
 ./_genonce.sh
